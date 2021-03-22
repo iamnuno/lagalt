d327ac5f2ba81ab3fb86a836a8cca3783c16aef7
Nuno Cunha <8029586-iamnuno@users.noreply.gitlab.com>
2021-03-22 13:33:11 +0100

    Feat: Adds vue-router with home view route

--- src/main.js
+++ src/main.js
@@ -1,20 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import routes from './router/index';
+import { BootstrapVue } from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-library.add(faUserCircle)
+library.add(faUserCircle);
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
+
+// Use VueRouter
+Vue.use(VueRouter);
+const router = new VueRouter({
+    routes,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+    router,
+}).$mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <ProjectBanner />
+</template>
+
+<script>
+import ProjectBanner from "../components/ProjectBanner";
+export default {
+  name: "Home",
+  components: { ProjectBanner },
+};
+</script>
+
+<style>
+</style>
