10d0ad485ae944a925f9f4599f35079bb84d0a87
Ahmad <ahmadabdulal@outlook.com>
2021-03-23 09:38:56 +0100

    Chore: importing new icons

--- src/main.js
+++ src/main.js
@@ -1,27 +1,20 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueRouter from 'vue-router';
-import routes from './router/index';
+import { router } from './router/index';
 import { BootstrapVue } from 'bootstrap-vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faSearch, faFilter } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-library.add(faUserCircle);
+library.add(faUserCircle, faSearch, faFilter);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
-
-// Use VueRouter
-Vue.use(VueRouter);
-const router = new VueRouter({
-    routes,
-});
 
 new Vue({
     render: (h) => h(App),
