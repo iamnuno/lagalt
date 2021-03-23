6433e72db33592d3720804a2bfec293c0f65b594
Ahmad <ahmadabdulal@outlook.com>
2021-03-23 12:07:53 +0100

    Feat: adds project card component.
    this card can be used for both projects' cards and announcements' cards in project user view

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-light d-flex flex-column p-2 m-2 rounded shadow pb-3">
+    <div class="d-flex flex-row">
+      <div class="flex-grow-1">{{ title }}</div>
+      {{ date }}
+    </div>
+    <div class="breakline" />
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "projectCard",
+  props: ["data"],
+  data() {
+    return {
+      title: this.data.title,
+      text: this.data.text,
+      date: this.data.date,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.breakline {
+  background-color: rgba(119, 119, 119, 0.418);
+  padding-top: 1px;
+  margin-top: 1px;
+}
+</style>
