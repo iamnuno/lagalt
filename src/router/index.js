97389498e03944c306625f0a4965ae73156c3e2c
Ahmad <ahmadabdulal@outlook.com>
2021-03-23 09:38:11 +0100

    Refactor: refactor routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
-import Home from '../views/Home';
+import Home from '../views/Home'
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home,
-    },
-];
+    { path: "/", name: "Home", component: Home }
+]
 
-export default routes;
+export const router = new VueRouter({
+    mode: 'history',
+    routes
+})
