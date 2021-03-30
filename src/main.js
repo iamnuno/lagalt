3a786563c89d4eeb6f67724e6e9cb96c1a632609
Ahmad <ahmadabdulal@outlook.com>
2021-03-30 08:16:29 +0200

    Chore: adds firebase dependencies

--- src/main.js
+++ src/main.js
@@ -18,6 +18,25 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import firebase from "firebase/app";
+import axios from 'axios'
+
+Vue.prototype.$axios = axios;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzwHI_Dpcsqt8YQLTvUk-MrfIdpUhWWWI",
+    authDomain: "lagalt-147b3.firebaseapp.com",
+    databaseURL: "https://lagalt-147b3-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "lagalt-147b3",
+    storageBucket: "lagalt-147b3.appspot.com",
+    messagingSenderId: "1090780834044",
+    appId: "1:1090780834044:web:4709295c1b1f6ff33f8f79",
+    measurementId: "G-XL3WFBW3W0"
+}
+
+firebase.initializeApp(firebaseConfig);
+
+
 library.add(
     faUserCircle,
     faSearch,
