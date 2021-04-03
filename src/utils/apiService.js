dd83524afcfd6fed792d97507fec44dc0f6bb7ab
Ahmad <ahmadabdulal@outlook.com>
2021-04-03 20:06:38 +0200

    Feat: adds more requests with database

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { API_URL } from "../constants/constants";
+import { BASE_API_URL, API_URL } from "../constants/constants";
+import { store } from './store';
 
 async function newUser(username, email) {
     let res;
-    await axios.post(API_URL + '/users', {
+    await axios.post(BASE_API_URL + API_URL + '/users', {
         userName: username,
         userEmail: email,
         userSkills: null
@@ -15,4 +16,42 @@ async function newUser(username, email) {
     return res;
 }
 
-export { newUser }
+async function getAvailableProjects() {
+    return axios
+        .get(BASE_API_URL + API_URL + '/projects')
+        .then((response) => { return response.data })
+        .catch((err) => alert(err));
+}
+
+async function getUserProjects() {
+    return axios
+        .get(BASE_API_URL + API_URL + '/projects')
+        .then((response) => { return response.data })
+        .catch((err) => alert(err));
+}
+
+async function getUser() {
+    return axios
+        .get(BASE_API_URL + API_URL + '/users/' + store.getters.userId)
+        .then((response) => { return response.data })
+        .catch((err) => alert(err));
+}
+
+async function getProjectById(id) {
+    return axios.get(BASE_API_URL + API_URL + '/projects/' + id)
+        .then((response) => { return response.data })
+        .catch((err) => alert(err));
+}
+
+async function getGitCommit(url) {
+    url = "https://api.github.com/repos/iamnuno/lagalt/commits";
+    return axios
+        .get(url)
+        .then((response) => { return response.data })
+        .catch((err) => alert(err));
+}
+
+async function getCollaborators(userUrl) {
+    return axios.get(BASE_API_URL + userUrl).then(response => { return response.data }).catch((err) => alert(err))
+}
+export { newUser, getAvailableProjects, getUserProjects, getUser, getProjectById, getGitCommit, getCollaborators }
