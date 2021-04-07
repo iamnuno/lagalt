add23e129abb8ef2bba7529bb671eaf0f049b342
Ahmad <ahmadabdulal@outlook.com>
2021-04-07 09:49:38 +0200

    Refactor(footer): text changed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="d-flex flex-row p-4 bg-info text-white footer">
-    Hello from footer
+    This application is built with Vue and powered by Spring boot.
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "Footer"
-}
-
+  name: "Footer",
+};
 </script>
 
 <style>
 .footer {
-  box-shadow: 0 -0.5rem 1rem rgba(0 ,0 ,0 , 0.15) !important;
+  box-shadow: 0 -0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
 }
 </style>
